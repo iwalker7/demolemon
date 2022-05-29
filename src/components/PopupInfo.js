c6a18f228adbab4d905a290161e34a4db9cf359d
itgel G <itgel.g@nmtec.co>
2022-05-29 21:42:04 +0800

    Commit made from VS Code Azure Static Web Apps

--- src/components/PopupInfo.js
+++ src/components/PopupInfo.js
@@ -6,56 +6,70 @@ class PopUpInfo extends HTMLElement {
         // Create a shadow root
         const shadow = this.attachShadow({ mode: "open" });
 
-        if (this.hasAttribute("for")) {
-            const wrapperId = this.getAttribute("for");
+        // Create spans
+        const wrapper = document.createElement("span");
+        wrapper.setAttribute("class", "wrapper");
 
-            const wrapper = document.getElementById(wrapperId);
+        const icon = document.createElement("span");
+        icon.setAttribute("class", "icon");
+        icon.setAttribute("tabindex", 0);
 
-            console.log("wrapperId", wrapperId);
+        const info = document.createElement("span");
+        info.setAttribute("class", "info");
 
-            const info = document.createElement("span");
-            info.setAttribute("class", "info");
+        // Take attribute content and put it inside the info span
+        const text = this.getAttribute("data-text");
+        info.textContent = text;
 
-            // Take attribute content and put it inside the info span
-            const text = this.getAttribute("data-text");
-            info.textContent = text;
-            wrapper.appendChild(info);
-
-            // Create some CSS to apply to the shadow dom
-            const style = document.createElement("style");
-
-            style.textContent = `
-                .wrapper {
-                    position: relative;
-                }
-                .info {
-                    font-size: 0.8rem;
-                    width: 200px;
-                    display: inline-block;
-                    border: 1px solid white;
-                    padding: 10px;
-                    background: white;
-                    border-radius: 10px;
-                    opacity: 1;
-                    transition: 0.6s all;
-                    position: absolute;
-                    bottom: 20px;
-                    left: 10px;
-                    z-index: 3;
-                }
-                img {
-                    width: 1.2rem;
-                }
-                #${wrapperId}:hover + .info, #${wrapperId}:focus + .info {
-                    opacity: 1;
-                    background: black;
-                }
-            `;
-
-            // Attach the created elements to the shadow dom
-            shadow.appendChild(style);
-            shadow.appendChild(wrapper);
+        // Insert icon
+        let imgUrl;
+        if (this.hasAttribute("img")) {
+            imgUrl = this.getAttribute("img");
+        } else {
+            imgUrl = "img/default.png";
         }
+
+        const img = document.createElement("img");
+        img.src = imgUrl;
+        icon.appendChild(img);
+
+        // Create some CSS to apply to the shadow dom
+        const style = document.createElement("style");
+        console.log(style.isConnected);
+
+        style.textContent = `
+        .wrapper {
+          position: relative;
+        }
+        .info {
+          font-size: 0.8rem;
+          width: 200px;
+          display: inline-block;
+          border: 1px solid black;
+          padding: 10px;
+          background: white;
+          border-radius: 10px;
+          opacity: 0;
+          transition: 0.6s all;
+          position: absolute;
+          bottom: 20px;
+          left: 10px;
+          z-index: 3;
+        }
+        img {
+          width: 1.2rem;
+        }
+        .icon:hover + .info, .icon:focus + .info {
+          opacity: 1;
+        }
+      `;
+
+        // Attach the created elements to the shadow dom
+        shadow.appendChild(style);
+        console.log(style.isConnected);
+        shadow.appendChild(wrapper);
+        wrapper.appendChild(icon);
+        wrapper.appendChild(info);
     }
 }
 
